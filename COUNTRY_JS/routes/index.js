596e24a8ea7bc6ea7c2d09c03a33156d7554d120
robertogarnero <garnero.roberto@iisgalvanimi.edu.it>
2019-12-03 11:54:16 +0000

    3

--- COUNTRY_JS/routes/index.js
+++ COUNTRY_JS/routes/index.js
@@ -1,13 +1,21 @@
+var createError = require('http-errors');
 var express = require('express');
 var router = express.Router();
-var country = require ('countryjs');
+var country = require('countryjs');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ res.render('index', { title: 'Express' });
 });
+
 router.get('/pages/:state/', function(req, res, next){
- res.render('state', {state: country.info(req.params.state)}) ; //Rendiamo lo stato un parametro
+
+ if (typeof country.info(req.params.state) === "undefined") {
+   return next(createError(422, 'OOPS! State not found'));
+ }
+ else
+ {
+   res.render('state', {state: country.info(req.params.state)});
+ }
 })
 
 module.exports = router;
