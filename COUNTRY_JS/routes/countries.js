ea358913fd635ca757dc5c9110627523cea2fefd
robertogarnero <garnero.roberto@iisgalvanimi.edu.it>
2019-12-03 11:38:58 +0000

    1

---
+++ COUNTRY_JS/routes/countries.js
@@ -0,0 +1,10 @@
+var express = require('express'); //Carica il modulo express
+var router = express.Router(); //Ottiene un oggetto router
+var country = require('countryjs');
+
+
+router.get('/info/:state/', function(req, res, next){ //Rendiamo lo stato un parametro
+ res.send(req.params);
+})
+
+module.exports = router; //esporta il modulo per poterlo usare in app.js
