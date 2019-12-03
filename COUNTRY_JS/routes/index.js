51b25ed92eab54a41802643195d902ab7df78670
robertogarnero <garnero.roberto@iisgalvanimi.edu.it>
2019-12-03 11:47:32 +0000

    2

--- COUNTRY_JS/routes/index.js
+++ COUNTRY_JS/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var country = require ('countryjs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/pages/:state/', function(req, res, next){
+ res.render('state', {state: country.info(req.params.state)}) ; //Rendiamo lo stato un parametro
+})
 
 module.exports = router;
